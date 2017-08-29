bf4c0243af308178a0561a2d9d15889a7835aa29
Swen Meeuwes <swenmeeu@gmail.com>
2017-08-29 22:30:25 +0200

    Implemented a custom editor for the 'OffScreenSpawners' spawner

---
+++ Game/Get Off Me/Assets/Scripts/Spawner/Editor/OffScreenSpawnerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(OffScreenSpawner))]
+public class OffScreenSpawnerEditor : Editor
+{
+    private OffScreenSpawner spawner;
+    private void OnEnable()
+    {
+        spawner = (OffScreenSpawner)serializedObject.targetObject;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.Label("Developer tools", EditorStyles.boldLabel);
+
+        if(GUILayout.Button("Kill all spawns"))
+            spawner.DestroyAllSpawns();
+    }
+}
