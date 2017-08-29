d98fc40aeaefa8e375a05bc80c655a0372055480
Swen Meeuwes <swenmeeu@gmail.com>
2017-08-29 21:53:44 +0200

    Implemented abstract spawner Implemented a spawner that spawns gameobjects offscreen

---
+++ Game/Get Off Me/Assets/Scripts/Spawner/OffScreenSpawner.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Spawns GameObject X out of the Main Camera's sight
+/// GameObjects are spawned in a radius around the camera's position
+/// </summary>
+public class OffScreenSpawner : AbstractSpawner
+{
+    // Exposed inspector fields
+    [SerializeField]
+    private GameObject objectToSpawn;
+    [SerializeField]
+    private Camera orthographicCamera;
+    [SerializeField]
+    private float spawnDistanceOffset = 0.0f;
+    [SerializeField]
+    private int amountOfSpawnPoints = 180;
+    // ---
+
+    private float OffsettedScreenDiagonal
+    {
+        get
+        {
+            var cameraHeight = orthographicCamera.orthographicSize * 2f;
+            var cameraWidth = cameraHeight * orthographicCamera.aspect;
+
+            var screenDiagonal = (float)Math.Sqrt(Math.Pow(cameraHeight, 2) + Math.Pow(cameraWidth, 2));
+
+            return screenDiagonal + spawnDistanceOffset;
+        }
+    }
+
+    private Vector2[] spawnPoints;
+
+    public override void Start()
+    {
+        base.Start();
+
+        if (orthographicCamera == null)
+            orthographicCamera = Camera.main;
+
+        InitializeSpawnPoints(amountOfSpawnPoints);
+    }
+
+    public override void Spawn()
+    {
+        var randomSpawnPosition = spawnPoints[UnityEngine.Random.Range(0, amountOfSpawnPoints - 1)];
+
+        var spawned = base.CreateSpawn(objectToSpawn);
+        spawned.transform.position = randomSpawnPosition;
+    }
+
+    /// <summary>
+    /// Initializes all spawn points on the spawn circle
+    /// </summary>
+    /// <param name="precision">Amount of spawn amounts</param>
+    private void InitializeSpawnPoints(int precision)
+    {
+        if (precision < 0)
+        {
+            spawnPoints = new Vector2[0];
+            return;
+        }
+            
+        var spawnCircleRadius = OffsettedScreenDiagonal / 2f;
+
+        var possibleSpawnPoints = new Vector2[precision];
+        for (int i = 0; i < precision; i++)
+        {
+            var degree = ((float)i / (float)precision) * 360f;
+            var radianAngle = degree * Mathf.Deg2Rad;
+
+            var x = (float)(orthographicCamera.transform.position.x + spawnCircleRadius * Math.Cos(radianAngle));
+            var y = (float)(orthographicCamera.transform.position.y + spawnCircleRadius * Math.Sin(radianAngle));
+
+            possibleSpawnPoints[i] = new Vector2(x, y);
+        }
+
+        spawnPoints = possibleSpawnPoints;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // While NOT in play-mode
+        if (!EditorApplication.isPlaying)
+            InitializeSpawnPoints(amountOfSpawnPoints);
+        // ---
+
+        if (orthographicCamera != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(orthographicCamera.transform.position, OffsettedScreenDiagonal / 2f);
+        }
+
+        if (spawnPoints != null)
+        {            
+            for (int i = 0; i < spawnPoints.Length; i++)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawCube(spawnPoints[i], Vector3.one * 0.2f);                
+            }
+        }
+    }
+}
