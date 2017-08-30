7c0006dddedf358b366fc3e67a170e5431c9d4ef
katjang <niek_o@hotmail.com>
2017-08-30 10:39:37 +0200

    added game scene with basic enemy AI

---
+++ Game/Get Off Me/Assets/Scripts/EnemyStandard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStandard : MonoBehaviour {
+
+    public GameObject target;
+    public float speed = 5f;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    }
+}
