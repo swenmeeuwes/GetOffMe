f820d8be28338a18c4d8d761704a926496d8bc52
katjang <niek_o@hotmail.com>
2017-08-30 15:34:18 +0200

    added enemy click / drag

--- Game/Get Off Me/Assets/Scripts/Enemies/AbstractEnemy.cs
+++ Game/Get Off Me/Assets/Scripts/Enemies/AbstractEnemy.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class AbstractEnemy : MonoBehaviour {
 
     protected int health;
-    protected float speed;
+    protected float maxSpeed;
     protected GameObject target;
+    protected Vector3 velocity;
+    protected float acceleration; // per second
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private Vector3 oldPosition = new Vector3(0,0,0);
 
     void Start() { }
 
     void Update() { }
+
+
+    void OnMouseDown()
+    {
+        oldPosition = transform.position;
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        oldPosition = transform.position;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+    void OnMouseUp() {
+        velocity = transform.position - oldPosition;
+    }
 }
--- Game/Get Off Me/Assets/Scripts/Enemies/EnemyStandard.cs
+++ Game/Get Off Me/Assets/Scripts/Enemies/EnemyStandard.cs
@@ -9,14 +9,18 @@ public class EnemyStandard : AbstractEnemy {
     // Use this for initialization
     void Start () {
         target = GameObject.FindWithTag("Player");
+        this.acceleration = 0.1f;
+        this.velocity = new Vector3(0, 0, 0);
+        this.maxSpeed = 0.1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float step = enemyModel.speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
 
-        
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        this.velocity += new Vector3(direction.x * this.acceleration * Time.deltaTime, direction.y * this.acceleration * Time.deltaTime, 0);
+        this.velocity = Vector3.ClampMagnitude(this.velocity, this.maxSpeed);
+        transform.position += this.velocity;
     }
 
     void OnCollisionEnter2D(Collision2D coll) {
