f6ec5d9a743b8db13eae3602772db7eac2e60029
Swen <swenmeeu@gmail.com>
2017-08-30 14:50:06 +0200

    Implemented scene loader

---
+++ Game/Get Off Me/Assets/Scripts/Application/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    [SerializeField]
+    private string sceneName;
+    [SerializeField][Tooltip("Time in seconds before the scene gets loaded")]
+    private float delay;
+
+	private void Start () {
+        Invoke("Execute", delay);
+	}
+
+    private void Execute()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
