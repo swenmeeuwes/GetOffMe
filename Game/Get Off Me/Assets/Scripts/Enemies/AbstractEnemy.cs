eff0f4455e2faf57448329e78daedf3ac91dcf96
Swen <swenmeeu@gmail.com>
2017-08-31 11:25:54 +0200

    Implemented velocity clamping

--- Game/Get Off Me/Assets/Scripts/Enemies/AbstractEnemy.cs
+++ Game/Get Off Me/Assets/Scripts/Enemies/AbstractEnemy.cs
@@ -9,14 +9,16 @@ public class AbstractEnemy : MonoBehaviour
 
     protected Enemy model;
     protected GameObject target;
-    protected Vector3 velocity;
-    protected float acceleration; // per second
-    protected float frictionAcceleration;
-    protected int weight;
+    protected float maxMagnitude = 0.4f; // put this in enemy model
+    //protected Vector3 velocity;
+    //protected float acceleration; // per second
+    //protected float frictionAcceleration;
+    //protected int weight; // put this in enemy model
 
     private Vector3 screenPoint;
     private Vector3 offset;
     private Vector3 oldPosition = Vector3.one;
+
     protected Rigidbody2D rb;
 
     protected virtual void Start()
@@ -46,7 +48,12 @@ public class AbstractEnemy : MonoBehaviour
     }
     void OnMouseUp()
     {
-        rb.velocity = (transform.position - oldPosition) * 50;
+        var newVelocity = (transform.position - oldPosition) * 10;
+        if(newVelocity.magnitude < 0)
+            rb.velocity = Vector3.ClampMagnitude(newVelocity, -20);
+        else if(newVelocity.magnitude > 0)
+            rb.velocity = Vector3.ClampMagnitude(newVelocity, 20);
+
         model.health -= 1;
         if (model.health <= 0)
             StartCoroutine(Die());
--- Game/Get Off Me/Assets/Scripts/Enemies/EnemyStandard.cs
+++ Game/Get Off Me/Assets/Scripts/Enemies/EnemyStandard.cs
@@ -9,13 +9,21 @@ public class EnemyStandard : AbstractEnemy {
 	}
 	
 	void Update () {
-        var velocityModifier = 1;
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        var outsideViewport = false;
         if (pos.x < 0 || pos.x > 1 || pos.y < 0 || pos.y > 1)
+            outsideViewport = true;
+
+        var velocityModifier = 1;
+        if (outsideViewport)
             velocityModifier = 10;
 
         Vector3 direction = (target.transform.position - transform.position).normalized * velocityModifier;
         rb.AddForce(direction);
+        if (!outsideViewport && rb.velocity.magnitude < maxMagnitude)
+        {
+            rb.AddForce(-direction * 1000);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D coll) {
