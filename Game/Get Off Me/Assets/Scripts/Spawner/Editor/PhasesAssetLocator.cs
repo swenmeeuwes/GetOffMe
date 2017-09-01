83830bd60b57ee4b4fad9921480425b388079fee
katjang <niek_o@hotmail.com>
2017-09-01 16:05:18 +0200

    locator for phase files

---
+++ Game/Get Off Me/Assets/Scripts/Spawner/Editor/PhasesAssetLocator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhasesAssetLocator {
+    private readonly string ASSET_SAVE_PATH = "Assets/Scripts/Spawner/Editor/Phases";
+
+    private static PhasesAssetLocator m_instance;
+    public static PhasesAssetLocator Instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new PhasesAssetLocator();
+
+            return m_instance;
+        }
+    }
+    public string ResolveFileName(string filename)
+    {
+        return ASSET_SAVE_PATH + "/" + filename + ".asset";
+    }
+    public string GetSavePath() {
+        return ASSET_SAVE_PATH;
+    }
+}
