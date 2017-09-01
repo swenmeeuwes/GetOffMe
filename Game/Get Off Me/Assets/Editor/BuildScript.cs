9fa8a7e165b5ab0eb743d1928958d8a9171ebff5
Swen <swenmeeu@gmail.com>
2017-09-01 15:45:48 +0200

    Changes to buildscript

--- Game/Get Off Me/Assets/Editor/BuildScript.cs
+++ Game/Get Off Me/Assets/Editor/BuildScript.cs
@@ -9,20 +9,20 @@ class BuildScript
     static string[] SCENES = FindEnabledEditorScenes();
 
     static string APP_NAME = PlayerSettings.productName;
-    static string TARGET_DIR = "target";
+    static string TARGET_DIR = "Target";
 
-    [MenuItem("Custom/CI/Build Mac OS X")]
-    static void PerformMacOSXBuild()
-    {
-        string target_dir = APP_NAME + ".app";
-        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel, BuildOptions.None);
-    }
+    //[MenuItem("Custom/CI/Build Mac OS X")]
+    //static void PerformMacOSXBuild()
+    //{
+    //    string target_dir = APP_NAME + ".app";
+    //    GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+    //}
 
     [MenuItem("Custom/CI/Build Android")]
     static void PerformAndroidBuild()
     {
         string target_dir = APP_NAME + ".apk";
-        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTargetGroup.Android, BuildTarget.Android, BuildOptions.None);
     }
 
     private static string[] FindEnabledEditorScenes()
@@ -36,11 +36,11 @@ class BuildScript
         return EditorScenes.ToArray();
     }
 
-    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    static void GenericBuild(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
     {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
-        if (res.Length > 0)
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        string res = BuildPipeline.BuildPlayer(scenes, targetDir, buildTarget, buildOptions);
+        if (res.Length > 0) // pls...
         {
             throw new Exception("BuildPlayer failure: " + res);
         }
