8fd90a1a8400ed4ca1f50f28391353cbcac20178
Swen <swenmeeu@gmail.com>
2017-09-01 12:24:20 +0200

    Implemented score scaffolding

---
+++ Game/Get Off Me/Assets/Scripts/Application/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager
+{
+    private static ScoreManager _instance;
+    public static ScoreManager Instance {
+        get {
+            if (_instance == null)
+                _instance = new ScoreManager();
+            return _instance;
+        }
+    }
+
+    private ScoreManager() {
+        Score = 0;
+    }
+
+    public int Score { get; set; }
+}
---
+++ Game/Get Off Me/Assets/Scripts/UI/ScoreText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreText : MonoBehaviour {
+    private Text textField;
+
+	void Start () {
+        textField = GetComponent<Text>();
+
+        textField.text = ScoreManager.Instance.Score.ToString();
+    }
+}
