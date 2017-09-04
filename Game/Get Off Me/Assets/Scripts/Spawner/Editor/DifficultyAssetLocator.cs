2e208d8ad1c54a95e407690d46d0d47d2b9944a1
katjang <niek_o@hotmail.com>
2017-09-04 12:28:12 +0200

    Enemy Editor

---
+++ Game/Get Off Me/Assets/Scripts/Spawner/Editor/DifficultyAssetLocator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyAssetLocator {
+    private readonly string PHASES_SAVE_PATH = "Assets/Scripts/Spawner/Editor/Phases";
+    private readonly string ENEMY_SAVE_PATH = "Assets/Scripts/Spawner/Editor/Enemies";
+
+    private static DifficultyAssetLocator m_instance;
+    public static DifficultyAssetLocator Instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new DifficultyAssetLocator();
+
+            return m_instance;
+        }
+    }
+    public string ResolveFileNamePhases(string filename)
+    {
+        return PHASES_SAVE_PATH + "/" + filename + ".asset";
+    }
+    public string ResolveFileNameEnemies()
+    {
+        return ENEMY_SAVE_PATH + "/" + "EnemyCollection" + ".asset";
+    }
+    public string GetPhaseSavePath() {
+        return PHASES_SAVE_PATH;
+    }
+    public string GetEnemySavePath() {
+        return ENEMY_SAVE_PATH + "/" + "EnemyCollection.asset";
+    }
+}
--- Game/Get Off Me/Assets/Scripts/Spawner/Editor/DifficultyManager.cs
+++ Game/Get Off Me/Assets/Scripts/Spawner/Editor/DifficultyManager.cs
@@ -1,76 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class DifficultyManager : EditorWindow {
-    public class GamePhase : ScriptableObject
-    {
-        public int time;
-        public float[] percentages;
+
+    public class EnemyCollection : ScriptableObject {
+        public List<GameObject> enemyTypes = new List<GameObject>();
     }
+
+
     [MenuItem("Window/Difficulty Editor")]
     public static void ShowWindow()
     {
         var window = EditorWindow.GetWindow(typeof(DifficultyManager));
         window.minSize = new Vector2(600, 400);
     }
-    private List<AbstractEntity> enemyTypes;
-    private int enemyTypesCount = 4;
 
     private List<GamePhase> gamePhases = new List<GamePhase>();
+    private EnemyCollection enemyCollection;
 
     void OnEnable() {
         handleLoad();
     }
     private void OnGUI() {
+        Debug.Log(gamePhases.Count);
+        for (int i = 0; i < gamePhases.Count; i++) {
+            if (gamePhases[i].percentages == null) gamePhases[i].percentages = new List<float>();
+            if (gamePhases[i].objectKeys == null) gamePhases[i].objectKeys = new List<GameObject>();
+        }
 
         GUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("Time");
-        for (int i = 0; i < enemyTypesCount; i++) {
-            EditorGUILayout.LabelField(" ");
+        for (int i = 0; i < enemyCollection.enemyTypes.Count; i++) {
+            EditorGUI.BeginChangeCheck();
+            enemyCollection.enemyTypes[i] = (GameObject)EditorGUILayout.ObjectField(enemyCollection.enemyTypes[i], typeof(GameObject), false);
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (enemyCollection.enemyTypes[i] != null)
+                {
+                    for (int j = 0; j < gamePhases.Count; j++)
+                    {
+                        gamePhases[j].percentages.Add(0);
+                        gamePhases[j].objectKeys.Add(enemyCollection.enemyTypes[i]);
+                    }
+                }
+            }
         }
         GUILayout.EndHorizontal();
 
-        for (int i = 0; i < gamePhases.Count; i ++) {
+        for (int i = 0; i < gamePhases.Count; i++) {
             GUILayout.BeginHorizontal();
+
             gamePhases[i].time = EditorGUILayout.IntField(gamePhases[i].time);
-            if (gamePhases[i].percentages == null) {
-                gamePhases[i].percentages = new float[enemyTypesCount];
-            }
-            for (int j = 0; j < gamePhases[i].percentages.Length; j++)
+            
+            for(int j =0; j < gamePhases[i].objectKeys.Count; j++)
             {
                 gamePhases[i].percentages[j] = EditorGUILayout.FloatField(gamePhases[i].percentages[j]);
             }
             GUILayout.EndHorizontal();
         }
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Add Phase"))
-            addPhase();
+        if (GUILayout.Button("Add Phase")) { addPhase(); }
+        if (GUILayout.Button("Add Entity")) { addEntity(); }
         GUILayout.EndHorizontal();
+
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("Save")) { handleSave();  }
-            
         GUILayout.EndHorizontal();
     }
     private void addPhase() {
-        gamePhases.Add(new GamePhase());
+        GamePhase tmp = new GamePhase();
+        tmp.percentages = new List<float>();
+        tmp.objectKeys = new List<GameObject>();
+
+        for (int i = 0; i < enemyCollection.enemyTypes.Count; i++)
+        {
+            if (enemyCollection.enemyTypes[i] != null)
+            {
+                tmp.percentages.Add(0);
+                tmp.objectKeys.Add(enemyCollection.enemyTypes[i]);
+            }
+        }
+        gamePhases.Add(tmp);
     }
     private void handleSave() {
         for (int i = 0; i < gamePhases.Count; i++)
         {
-            AssetDatabase.CreateAsset(gamePhases[i], PhasesAssetLocator.Instance.ResolveFileName("phase_" + i));
+            AssetDatabase.CreateAsset(gamePhases[i], DifficultyAssetLocator.Instance.ResolveFileNamePhases("phase_" + i));
         }
+        AssetDatabase.CreateAsset(enemyCollection, DifficultyAssetLocator.Instance.ResolveFileNameEnemies());
     }
     private void handleLoad() {
-        string[] aMaterialFiles = Directory.GetFiles(PhasesAssetLocator.Instance.GetSavePath(), "*.asset", SearchOption.AllDirectories);
+        string[] aMaterialFiles = Directory.GetFiles(DifficultyAssetLocator.Instance.GetPhaseSavePath(), "*.asset", SearchOption.AllDirectories);
         foreach (string matFile in aMaterialFiles)
         {
             string assetPath = matFile.Replace(Application.dataPath, "").Replace('\\', '/');
             var sourceMat = AssetDatabase.LoadAssetAtPath<GamePhase>(assetPath);
-            
+
             gamePhases.Add(sourceMat);
         }
+        enemyCollection = AssetDatabase.LoadAssetAtPath<EnemyCollection>(DifficultyAssetLocator.Instance.ResolveFileNameEnemies());
+        if (enemyCollection == null) {
+            enemyCollection = new EnemyCollection();
+        }
+
+    }
+    private void addEntity() {
+        enemyCollection.enemyTypes.Add(null);
     }
 }
