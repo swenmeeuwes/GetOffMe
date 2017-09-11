aa737461f166d50698acb957968c9ff74281baba
Swen <swenmeeu@gmail.com>
2017-09-11 15:47:39 +0200

    Implemented medic ally

---
+++ Game/Get Off Me/Assets/Scripts/Enemies/MedicSlimeAlly.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicSlimeAlly : SeekingEntity {
+    public override void OnPlayerHit(Player player)
+    {
+        player.Heal(1);
+        base.OnPlayerHit(player);
+    }
+}
