0f98c844f0b02db55d6ff32165da7d81a8425fd7
Swen <swenmeeu@gmail.com>
2017-09-11 11:52:33 +0200

    Scaffolded google play services prompt panel

---
+++ Game/Get Off Me/Assets/Scripts/UI/GooglePlayLoginPromptPopup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooglePlayLoginPromptPopup : Panel
+{
+    public void HandleContinueButton()
+    {
+
+        Deactiveate();
+    }
+
+    public void HandleCancelButton()
+    {
+        Deactiveate();
+    }
+}
