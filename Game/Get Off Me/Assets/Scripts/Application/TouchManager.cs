daa00ee5dd41692b435fe4be2ba4ba1f4a21bc34
Swen Meeuwes <swenmeeu@gmail.com>
2017-09-20 09:40:48 +0200

    First iteration touch manager

---
+++ Game/Get Off Me/Assets/Scripts/Application/TouchManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+	private void Update () {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            var touch = Input.GetTouch(i);
+            var worldPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            var hits = Physics2D.CircleCastAll(worldPosition, 0.2f, Vector3.forward);
+            for (int j = 0; j < hits.Length; j++)
+            {
+                var hit = hits[j];
+                HandleTouch(touch, hit.transform);
+            }
+        }
+	}
+
+    private void HandleTouch(Touch touch, Transform transform)
+    {
+        if (transform == null)
+            return;
+        
+        switch(touch.phase)
+        {
+            case TouchPhase.Began:
+                transform.gameObject.SendMessage("OnTouchBegan", touch);
+                break;
+            case TouchPhase.Moved:
+            case TouchPhase.Stationary:
+                transform.gameObject.SendMessage("OnTouch", touch);
+                break;
+            case TouchPhase.Ended:
+                transform.gameObject.SendMessage("OnTouchEnded", touch);
+                break;
+        }        
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            var touch = Input.GetTouch(i);
+            var worldPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(worldPosition, 0.2f);
+        }
+    }
+}
