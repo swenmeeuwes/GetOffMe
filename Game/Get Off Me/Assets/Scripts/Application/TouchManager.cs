df2aef050da4584f189fe69927065d6c5a33e3b6
Swen Meeuwes <swenmeeu@gmail.com>
2017-09-20 11:35:55 +0200

    Implemented ITouchable in Touchmanager

--- Game/Get Off Me/Assets/Scripts/Application/TouchManager.cs
+++ Game/Get Off Me/Assets/Scripts/Application/TouchManager.cs
@@ -3,42 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchManager : MonoBehaviour {
+    // Inspector variables
     [SerializeField][Tooltip("The radius of the circle scanned around the touches.")]
     private float castSphereRadius = 0.2f;
+    // ---
 
-	private void Update () {
+    public static TouchManager Main {
+        get {
+            return GameObject.FindGameObjectWithTag("MainTouchManager").GetComponent<TouchManager>();
+        }
+    }
+
+    private List<ITouchable> registeredTouchables;
+
+    public void Register(ITouchable touchable)
+    {
+        registeredTouchables.Add(touchable);
+    }
+
+    public void Deregister(ITouchable touchable)
+    {
+        registeredTouchables.Remove(touchable);
+    }
+
+    private void Awake()
+    {
+        registeredTouchables = new List<ITouchable>();
+    }
+
+    private void Update () {
         for (int i = 0; i < Input.touchCount; i++)
         {
             var touch = Input.GetTouch(i);
-            var worldPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            HandleTouch(touch);
 
+            var worldPosition = Camera.main.ScreenToWorldPoint(touch.position);
             var hits = Physics2D.CircleCastAll(worldPosition, castSphereRadius, Vector3.forward);
             for (int j = 0; j < hits.Length; j++)
             {
                 var hit = hits[j];
-                HandleTouch(touch, hit.transform);
+                HandleTouchOn(hit.transform, touch);                
             }
         }
 	}
 
-    private void HandleTouch(Touch touch, Transform transform)
+    private void HandleTouchOn(Transform transform, Touch touch)
     {
         if (transform == null)
             return;
-        
-        switch(touch.phase)
+
+        var touched = transform.gameObject;
+
+        if (touch.phase == TouchPhase.Began)
         {
-            case TouchPhase.Began:
-                transform.gameObject.SendMessage("OnTouchBegan", touch, SendMessageOptions.DontRequireReceiver);
-                break;
-            case TouchPhase.Moved:
-            case TouchPhase.Stationary:
-                transform.gameObject.SendMessage("OnTouch", touch, SendMessageOptions.DontRequireReceiver);
-                break;
-            case TouchPhase.Ended:
-                transform.gameObject.SendMessage("OnTouchEnded", touch, SendMessageOptions.DontRequireReceiver);
-                break;
-        }        
+            var touchable = touched.GetComponent<ITouchable>();
+            if (touchable == null)
+                return;
+
+            // Tag the ITouchable with the fingerId so we can call OnTouch and OnTouchEnded on the same object later
+            touchable.FingerId = touch.fingerId;
+
+            touchable.OnTouchBegan(touch);
+        }
+    }
+
+    private void HandleTouch(Touch touch)
+    {
+        foreach (var touchable in registeredTouchables)
+        {
+            if (touchable.FingerId == null)
+                continue;
+
+            switch(touch.phase)
+            {
+                case TouchPhase.Moved:
+                case TouchPhase.Stationary:
+                    touchable.OnTouch(touch);
+                    break;
+                case TouchPhase.Ended:
+                    touchable.OnTouchEnded(touch);
+
+                    // Remove tag so that the events won't fire anymore
+                    if(touchable.FingerId == touch.fingerId)
+                        touchable.FingerId = null;
+                    break;
+            }
+        }
     }
 
     private void OnDrawGizmos()
