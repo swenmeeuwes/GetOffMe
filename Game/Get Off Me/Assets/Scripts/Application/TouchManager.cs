c07ad62c8b0587577aad28c608b8a3aad0ad409a
Swen Meeuwes <swenmeeu@gmail.com>
2017-09-20 09:44:07 +0200

    Made cast sphere radius a variable available in the inspector

--- Game/Get Off Me/Assets/Scripts/Application/TouchManager.cs
+++ Game/Get Off Me/Assets/Scripts/Application/TouchManager.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchManager : MonoBehaviour {
-
+    [SerializeField][Tooltip("The radius of the circle scanned around the touches.")]
+    private float castSphereRadius = 0.2f;
 	private void Update () {
         for (int i = 0; i < Input.touchCount; i++)
         {
             var touch = Input.GetTouch(i);
             var worldPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
-            var hits = Physics2D.CircleCastAll(worldPosition, 0.2f, Vector3.forward);
+            var hits = Physics2D.CircleCastAll(worldPosition, castSphereRadius, Vector3.forward);
             for (int j = 0; j < hits.Length; j++)
             {
                 var hit = hits[j];
