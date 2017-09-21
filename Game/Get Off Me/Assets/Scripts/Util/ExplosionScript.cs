85aa8db2930f1dad5968741aeee05a3b8f94b668
katjang <niek_o@hotmail.com>
2017-09-21 15:19:16 +0200

    bomb enemy prefab (done)

---
+++ Game/Get Off Me/Assets/Scripts/Util/ExplosionScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour {
+
+    // Use this for initialization
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        AbstractEntity entity = coll.gameObject.GetComponent<AbstractEntity>();
+        if (entity)
+            StartCoroutine(entity.Die());
+    }
+
+}
