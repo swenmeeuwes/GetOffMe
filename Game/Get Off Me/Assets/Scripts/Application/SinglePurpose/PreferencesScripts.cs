59d8a3b5d7617030445713dae2ebc4b4cf6d5f3d
Swen <swenmeeu@gmail.com>
2017-09-25 16:01:48 +0200

    Helped a confused toggle button, trying to toggle itself on and off and off and on. He has found his purpose in life again.

--- Game/Get Off Me/Assets/Scripts/Application/SinglePurpose/PreferencesScripts.cs
+++ Game/Get Off Me/Assets/Scripts/Application/SinglePurpose/PreferencesScripts.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PreferencesScripts : MonoBehaviour {
+    private Toggle toggle;
     public void Start() {
-        GetComponent<UnityEngine.UI.Toggle>().isOn = PlayerPrefs.GetInt("ShowTutorial") ==1;
+        toggle = GetComponent<Toggle>();
+        
+        toggle.isOn = PlayerPrefs.GetInt("ShowTutorial") == 1;
     }
 
     public void EnableTutorial()
     {
         PlayerPrefs.SetInt("ShowTutorial", 1);
     }
+
     public void ToggleTutorial() {
-        PlayerPrefs.SetInt("ShowTutorial", PlayerPrefs.GetInt("ShowTutorial")==1?0:1);
+        PlayerPrefs.SetInt("ShowTutorial", toggle.isOn ? 1 : 0);
     }
 }
