49b5f2f8dde18677fe0ae6cd522d9d1161fa0446
katjang <niek_o@hotmail.com>
2017-09-25 15:49:04 +0200

    tutorial toggle

--- Game/Get Off Me/Assets/Scripts/Application/SinglePurpose/PreferencesScripts.cs
+++ Game/Get Off Me/Assets/Scripts/Application/SinglePurpose/PreferencesScripts.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PreferencesScripts : MonoBehaviour {
+    public void Start() {
+        GetComponent<UnityEngine.UI.Toggle>().isOn = PlayerPrefs.GetInt("ShowTutorial") ==1;
+    }
+
     public void EnableTutorial()
     {
         PlayerPrefs.SetInt("ShowTutorial", 1);
+    }
+    public void ToggleTutorial() {
+        PlayerPrefs.SetInt("ShowTutorial", PlayerPrefs.GetInt("ShowTutorial")==1?0:1);
     }
 }
