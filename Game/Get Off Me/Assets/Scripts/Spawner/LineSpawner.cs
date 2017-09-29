050f9a08e750b962c1e16fce47104eb710a41e85
Swen Meeuwes <swenmeeu@gmail.com>
2017-09-29 18:40:35 +0200

    Pimped the start menu with line spawners

---
+++ Game/Get Off Me/Assets/Scripts/Spawner/LineSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineSpawner : AbstractSpawner {
+    [SerializeField]
+    private GameObject[] spawnList;
+    [SerializeField]
+    private Vector3 p1;
+    [SerializeField]
+    private Vector3 p2;
+
+    public override void Spawn()
+    {
+        var spawn = base.CreateSpawn(spawnList.RandomItem());
+        spawn.transform.position = Vector3.Lerp(p1, p2, Random.value);
+
+        base.Spawn();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (p1 == null || p2 == null)
+            return;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(p1, p2);
+    }
+}
