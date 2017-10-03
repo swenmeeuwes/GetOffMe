8f0fefef2388ee386428c68dc82e672ad63a6b7d
Swen Meeuwes <swenmeeu@gmail.com>
2017-10-03 20:40:47 +0200

    Animated vial unlock panel

--- Game/Get Off Me/Assets/Scripts/UI/UnlockedVialPanel.cs
+++ Game/Get Off Me/Assets/Scripts/UI/UnlockedVialPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Animator))]
 public class UnlockedVialPanel : MonoBehaviour {
 
     public static UnlockedVialPanel Instance;
@@ -12,6 +13,8 @@ public class UnlockedVialPanel : MonoBehaviour {
     public Text negativeText;
     public Text description;
 
+    private Animator animator;
+
     // Use this for initialization
     void Awake() {
         if (Instance != null)
@@ -20,6 +23,8 @@ public class UnlockedVialPanel : MonoBehaviour {
         Instance = this;
     }
     void Start() {
+        animator = GetComponent<Animator>();
+
         gameObject.SetActive(false);
     }
     public void ShowUnlockedVial(VialData vial) {
@@ -29,8 +34,11 @@ public class UnlockedVialPanel : MonoBehaviour {
         negativeText.text = vial.negativeEffect;
         description.text = vial.description;
         gameObject.SetActive(true);
+
+        animator.SetTrigger("SlideIn");
     }
     public void NextPopup() {
+        animator.SetTrigger("SlideOut");
         GameManager.Instance.GameOverNextPanel();
     }
 }
