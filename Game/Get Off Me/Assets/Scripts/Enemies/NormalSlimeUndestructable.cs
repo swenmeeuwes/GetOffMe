8a531aef4bab5d38c4a3f0fa5d700d2ffaa0492b
katjang <niek_o@hotmail.com>
2017-10-03 14:09:08 +0200

    undestructable enemy

--- Game/Get Off Me/Assets/Scripts/Enemies/NormalSlimeUndestructable.cs
+++ Game/Get Off Me/Assets/Scripts/Enemies/NormalSlimeUndestructable.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class NormalSlimeUndestructable : NormalSlimeEnemy {
 
-	// Use this for initialization
     protected override void Awake()
     {
         base.Awake();
         entityType = EntityType.SLIME_NORMAL_TUTORIAL;
         IgnoreTap = true;
         Draggable = false;
+        ShowParticles = false;
     }
 }
