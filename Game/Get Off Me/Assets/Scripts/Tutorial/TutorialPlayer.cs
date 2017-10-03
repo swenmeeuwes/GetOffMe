fb1b2d410c09605fdc47c2b75b42b90291d5ed92
Swen <swenmeeu@gmail.com>
2017-10-03 14:22:40 +0200

    Show correct action text under enemies in tutorial

--- Game/Get Off Me/Assets/Scripts/Tutorial/TutorialPlayer.cs
+++ Game/Get Off Me/Assets/Scripts/Tutorial/TutorialPlayer.cs
@@ -152,14 +152,18 @@ public class TutorialPlayer : MonoBehaviour {
         instructionCanvas.transform.SetParent(spawned.transform, false);
 
 
-        if (spawnedEntity is HelmetSlimeEnemy)
+        if (!spawnedEntity.IgnoreTap)
         {
             instructionCanvas.GetComponentInChildren<Text>().text = "Tap";
             spawnedEntity.AddEventListener("tapped", (e) => instructionCanvas.GetComponentInChildren<Text>().text = "Swipe", true);
         }
-        else if (!(spawnedEntity is NormalSlimeUndestructable))
+        else if (spawnedEntity.Draggable)
         {
             instructionCanvas.GetComponentInChildren<Text>().text = "Swipe";
+        }
+        else
+        {
+            instructionCanvas.GetComponentInChildren<Text>().text = "";
         }
 
         if(spawnedEntity is MedicSlimeAlly)
